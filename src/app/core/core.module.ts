2d1ca110ce4c789d19d0d733d6d163cece4a5776
Erwin <erwinn@NEWARKNJ.GOV>
2019-12-05 13:56:29 -0500

    next

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,101 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  HttpClientModule
+} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import {
+  RouterStateSerializer,
+  StoreRouterConnectingModule
+} from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { environment } from '../../environments/environment';
+
+import {
+  AppState,
+  metaReducers,
+  reducers,
+  selectRouterState
+} from './core.state';
+
+import { AuthGuardService } from './auth/auth-guard.service';
+import { authLogin, authLogout } from './auth/auth.actions';
+import { AuthEffects } from './auth/auth.effects';
+import { selectAuth, selectIsAuthenticated } from './auth/auth.selectors';
+import { TitleService } from './title/title.service';
+
+import { AnimationsService } from './animations/animations.service';
+import {
+  ROUTE_ANIMATIONS_ELEMENTS,
+  routeAnimations
+} from './animations/route.animations';
+import { HttpErrorInterceptor } from './http-interceptors/http-error.interceptor';
+import { LocalStorageService } from './local-storage/local-storage.service';
+import { CustomSerializer } from './router/custom-serializer';
+import { SettingsEffects } from './settings/settings.effects';
+import {
+  selectEffectiveTheme,
+  selectSettingsLanguage,
+  selectSettingsStickyHeader
+} from './settings/settings.selectors';
+
+export {
+  TitleService,
+  selectAuth,
+  authLogin,
+  authLogout,
+  routeAnimations,
+  AppState,
+  LocalStorageService,
+  selectIsAuthenticated,
+  ROUTE_ANIMATIONS_ELEMENTS,
+  AnimationsService,
+  AuthGuardService,
+  selectRouterState,
+  selectEffectiveTheme,
+  selectSettingsLanguage,
+  selectSettingsStickyHeader
+};
+
+export const httpLoaderFactory = (http: HttpClient) =>
+  new TranslateHttpLoader(
+    http,
+    `${environment.config.i18nPrefix}/assets/i18n/`,
+    '.json'
+  );
 
 @NgModule({
+  exports: [TranslateModule],
   imports: [
-    CommonModule
+    // angular
+    CommonModule,
+    HttpClientModule,
+
+    // ngrx
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreRouterConnectingModule.forRoot(),
+    EffectsModule.forRoot([
+      AuthEffects,
+      SettingsEffects
+    ]),
+
+    // 3rd party
+    TranslateModule.forRoot({
+      loader: {
+        deps: [HttpClient],
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory
+      }
+    })
   ],
-  declarations: []
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
+    { provide: RouterStateSerializer, useClass: CustomSerializer }
+  ]
 })
 export class CoreModule { }
