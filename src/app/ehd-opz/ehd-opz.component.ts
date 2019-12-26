252ca0ca27931f458a683eadb40b1c4632b9c3c2
Newark-Planning <erwinn@ci.newark.nj.us>
2019-12-26 08:59:10 -0500

    hi

--- src/app/ehd-opz/ehd-opz.component.ts
+++ src/app/ehd-opz/ehd-opz.component.ts
@@ -19,10 +19,27 @@ export class EhdOpzComponent {
       .filter(route => route.data)
       .map(route => {
         const routedata = route.data ? route.data : '';
+        const routeChildren =
+          route.children ?
+          route.children.filter(child => child.data)
+            .map(childroute => {
+              const childroutedata = childroute.data ? childroute.data : '';
+              const icon = 'icon';
+              const title = 'title';
+
+              return {
+                icon: childroutedata[icon],
+                path: childroute.path,
+                title: childroutedata[title]
+              };
+            }
+          )
+          : undefined;
         const icon = 'icon';
         const title = 'title';
 
         return {
+          children: routeChildren,
           icon: routedata[icon],
           path: route.path,
           title: routedata[title]
--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 
 export * from './airtable-model';
 export interface MyRoute extends Route {
+  children?: undefined | Array<MyRoute>;
   icon?: string;
   title?: string;
 }
