e1557119cc3a3a5d03ceb7fa2142e8639fdd6b6c
Newark-Planning <erwinn@ci.newark.nj.us>
2020-01-04 20:35:33 -0500

    parcels still

--- src/assets/data/parcels.ts
+++ src/assets/data/parcels.ts
@@ -3,7 +3,6 @@ import * as parcels from './parcels';
 const parcelSource = (sourceData: any, map: any) => {
     map.addSource('parcels', {
         data: sourceData,
-        generateId: true,
         type: 'geojson'
     });
     map.addLayer({
@@ -37,64 +36,64 @@ const parcelSource = (sourceData: any, map: any) => {
 export const loadlayer = (name: any, map: any) => {
     switch (name) {
         case 'Belmont':
-            parcelSource(parcels.parcelLayers.parcels_belmont, map);
+            parcelSource(parcels.layer_parcels_belmont, map);
             break;
         case 'Dayton':
-            parcelSource(parcels.parcelLayers.parcels_dayton, map);
+            parcelSource(parcels.layer_parcels_dayton, map);
             break;
         case 'Downtown':
-            parcelSource(parcels.parcelLayers.parcels_downtown, map);
+            parcelSource(parcels.layer_parcels_downtown, map);
             break;
         case 'Fairmount':
-            parcelSource(parcels.parcelLayers.parcels_fairmount, map);
+            parcelSource(parcels.layer_parcels_fairmount, map);
             break;
         case 'Forest Hill':
-            parcelSource(parcels.parcelLayers.parcels_forest_hill, map);
+            parcelSource(parcels.layer_parcels_forest_hill, map);
             break;
         case 'Ironbound':
-            parcelSource(parcels.parcelLayers.parcels_ironbound, map);
+            parcelSource(parcels.layer_parcels_ironbound, map);
             break;
         case 'Lincoln Park':
-            parcelSource(parcels.parcelLayers.parcels_lincoln_park, map);
+            parcelSource(parcels.layer_parcels_lincoln_park, map);
             break;
         case 'Lower Broadway':
-            parcelSource(parcels.parcelLayers.parcels_lower_broadway, map);
+            parcelSource(parcels.layer_parcels_lower_broadway, map);
             break;
         case 'Lower Clinton Hill':
-            parcelSource(parcels.parcelLayers.parcels_lower_clinton_hill, map);
+            parcelSource(parcels.layer_parcels_lower_clinton_hill, map);
             break;
         case 'Lower Roseville':
-            parcelSource(parcels.parcelLayers.parcels_lower_roseville, map);
+            parcelSource(parcels.layer_parcels_lower_roseville, map);
             break;
         case 'Mount Pleasant':
-            parcelSource(parcels.parcelLayers.parcels_mount_pleasant, map);
+            parcelSource(parcels.layer_parcels_mount_pleasant, map);
             break;
         case 'Newark Industrial District':
-            parcelSource(parcels.parcelLayers.parcels_newark_industrial_district, map);
+            parcelSource(parcels.layer_parcels_newark_industrial_district, map);
             break;
         case 'North Broadway-Woodside':
-            parcelSource(parcels.parcelLayers.parcels_north_broadway_woodside, map);
+            parcelSource(parcels.layer_parcels_north_broadway_woodside, map);
             break;
         case 'Port District':
-            parcelSource(parcels.parcelLayers.parcels_port_district, map);
+            parcelSource(parcels.layer_parcels_port_district, map);
             break;
         case 'University Heights':
-            parcelSource(parcels.parcelLayers.parcels_university_heights, map);
+            parcelSource(parcels.layer_parcels_university_heights, map);
             break;
         case 'Upper Clinton Hill':
-            parcelSource(parcels.parcelLayers.parcels_upper_clinton_hill, map);
+            parcelSource(parcels.layer_parcels_upper_clinton_hill, map);
             break;
         case 'Upper Roseville':
-            parcelSource(parcels.parcelLayers.parcels_upper_roseville, map);
+            parcelSource(parcels.layer_parcels_upper_roseville, map);
             break;
         case 'Vailsburg':
-            parcelSource(parcels.parcelLayers.parcels_vailsburg, map);
+            parcelSource(parcels.layer_parcels_vailsburg, map);
             break;
         case 'Weequahic':
-            parcelSource(parcels.parcelLayers.parcels_weequahic, map);
+            parcelSource(parcels.layer_parcels_weequahic, map);
             break;
         case 'West Side':
-            parcelSource(parcels.parcelLayers.parcels_west_side, map);
+            parcelSource(parcels.layer_parcels_west_side, map);
             break;
         default:
             return {};
--- src/assets/data/parcels/index.ts
+++ src/assets/data/parcels/index.ts
@@ -1,46 +1,45 @@
-    // tslint:disable
-import { parcels_belmont } from './layer_parcels_Belmont';
-import { parcels_dayton } from './layer_parcels_Dayton';
-import { parcels_downtown } from './layer_parcels_Downtown';
-import { parcels_fairmount } from './layer_parcels_Fairmount';
-import { parcels_forest_hill } from './layer_parcels_Forest_Hill';
-import { parcels_ironbound } from './layer_parcels_Ironbound';
-import { parcels_lincoln_park } from './layer_parcels_Lincoln_Park';
-import { parcels_lower_broadway } from './layer_parcels_Lower_Broadway';
-import { parcels_lower_clinton_hill } from './layer_parcels_Lower_Clinton_Hill';
-import { parcels_lower_roseville } from './layer_parcels_Lower_Roseville';
-import { parcels_mount_pleasant } from './layer_parcels_Mount_Pleasant';
-import { parcels_newark_industrial_district } from './layer_parcels_Newark_Industrial_District';
-import { parcels_north_broadway_woodside } from './layer_parcels_North_Broadway-Woodside';
-import { parcels_port_district } from './layer_parcels_Port_District';
-import { parcels_university_heights } from './layer_parcels_University_Heights';
-import { parcels_upper_clinton_hill } from './layer_parcels_Upper_Clinton_Hill';
-import { parcels_upper_roseville } from './layer_parcels_Upper_Roseville';
-import { parcels_vailsburg } from './layer_parcels_Vailsburg';
-import { parcels_weequahic } from './layer_parcels_Weequahic';
-import { parcels_west_side } from './layer_parcels_West_side';
+import { layer_parcels_belmont } from './layer_parcels_Belmont';
+import { layer_parcels_dayton } from './layer_parcels_Dayton';
+import { layer_parcels_downtown } from './layer_parcels_Downtown';
+import { layer_parcels_fairmount } from './layer_parcels_Fairmount';
+import { layer_parcels_forest_hill } from './layer_parcels_Forest_Hill';
+import { layer_parcels_ironbound } from './layer_parcels_Ironbound';
+import { layer_parcels_lincoln_park } from './layer_parcels_Lincoln_Park';
+import { layer_parcels_lower_broadway } from './layer_parcels_Lower_Broadway';
+import { layer_parcels_lower_clinton_hill } from './layer_parcels_Lower_Clinton_Hill';
+import { layer_parcels_lower_roseville } from './layer_parcels_Lower_Roseville';
+import { layer_parcels_mount_pleasant } from './layer_parcels_Mount_Pleasant';
+import { layer_parcels_newark_industrial_district } from './layer_parcels_Newark_Industrial_District';
+import { layer_parcels_north_broadway_woodside } from './layer_parcels_North_Broadway-Woodside';
+import { layer_parcels_port_district } from './layer_parcels_Port_District';
+import { layer_parcels_university_heights } from './layer_parcels_University_Heights';
+import { layer_parcels_upper_clinton_hill } from './layer_parcels_Upper_Clinton_Hill';
+import { layer_parcels_upper_roseville } from './layer_parcels_Upper_Roseville';
+import { layer_parcels_vailsburg } from './layer_parcels_Vailsburg';
+import { layer_parcels_weequahic } from './layer_parcels_Weequahic';
+import { layer_parcels_west_side } from './layer_parcels_West_Side';
 
 export const parcelLayers = [
-    parcels_belmont,
-    parcels_dayton,
-    parcels_downtown,
-    parcels_fairmount,
-    parcels_forest_hill,
-    parcels_ironbound,
-    parcels_lincoln_park,
-    parcels_lower_broadway,
-    parcels_lower_clinton_hill,
-    parcels_lower_roseville,
-    parcels_mount_pleasant,
-    parcels_newark_industrial_district,
-    parcels_north_broadway_woodside,
-    parcels_port_district,
-    parcels_university_heights,
-    parcels_upper_clinton_hill,
-    parcels_upper_roseville,
-    parcels_vailsburg,
-    parcels_weequahic,
-    parcels_west_side
+   layer_parcels_belmont,
+   layer_parcels_dayton,
+   layer_parcels_downtown,
+   layer_parcels_fairmount,
+   layer_parcels_forest_hill,
+   layer_parcels_ironbound,
+   layer_parcels_lincoln_park,
+   layer_parcels_lower_broadway,
+   layer_parcels_lower_clinton_hill,
+   layer_parcels_lower_roseville,
+   layer_parcels_mount_pleasant,
+   layer_parcels_newark_industrial_district,
+   layer_parcels_north_broadway_woodside,
+   layer_parcels_port_district,
+   layer_parcels_university_heights,
+   layer_parcels_upper_clinton_hill,
+   layer_parcels_upper_roseville,
+   layer_parcels_vailsburg,
+   layer_parcels_weequahic,
+   layer_parcels_west_side
 ];
 
 export * from './layer_parcels_Belmont';
